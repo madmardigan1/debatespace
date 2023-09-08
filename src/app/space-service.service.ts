e76cf7b43b22e2decc6e8f1424fa81e20c3c517f
steve <brinkley0413@gmail.com>
2023-09-07 22:11:42 -0700

    u/i upgrades

--- src/app/space-service.service.ts
+++ src/app/space-service.service.ts
@@ -13,7 +13,18 @@ export interface Card {
 })
 
 export class CardDataService {
-  private cards = new BehaviorSubject<Card[]>([]);
+  private cards = new BehaviorSubject<Card[]>([
+    {id: '1', topic: "Politics", description: "What Trump just did", number: 10},
+    {id: '2', topic: "Technology", description: "Latest breakthroughs in AI", number: 50},
+    {id: '3', topic: "Science", description: "Mars rover finds something interesting", number: 20},
+    {id: '4', topic: "Sports", description: "Olympics highlights", number: 25},
+    {id: '5', topic: "Environment", description: "Climate change and its effects", number: 30},
+    {id: '6', topic: "Economy", description: "Market trends for this month", number: 40},
+    {id: '7', topic: "Arts", description: "Review of a popular art gallery", number: 15},
+    {id: '8', topic: "Lifestyle", description: "Trending fashions for this year", number: 22},
+    {id: '9', topic: "Travel", description: "Best destinations for post-pandemic travel", number: 35},
+    {id: '10', topic: "Health", description: "Tips for a healthy lifestyle", number: 28}
+  ]);
   cards$ = this.cards.asObservable();
 
   addCard(card: Card) {
