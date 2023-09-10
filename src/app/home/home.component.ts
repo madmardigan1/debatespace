1638274e80506d5cb568ddab60c0cc9dc5cdb0ea
steve <brinkley0413@gmail.com>
2023-09-10 14:58:53 -0700

    match-maker update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,26 @@
 import { Component} from '@angular/core';
 import { CardDataService, Card } from '../space-service.service';
 import { DebateAuthService } from '../debate-auth.service';
-
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   
 })
 export class HomeComponent{
+  userForm: FormGroup;
+  matchWindow = false;
   items: string[] = [];
   cards: Card[] = [];
-  constructor(private cardService: CardDataService, private debateAuth: DebateAuthService) {
+  constructor(private cardService: CardDataService, private debateAuth: DebateAuthService, private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      roles: this.fb.group({
+        speaker: [false],
+        host: [false]
+      }),
+      topics: this.fb.array([])
+    });
+    
     this.cardService.cards$.subscribe((data) => {
       this.cards = data;
     });
@@ -20,4 +30,20 @@ authorize(typeofUser: string) {
   this.debateAuth.setUser(typeofUser);
 }
  
+findMatch() : void {
+  this.matchWindow = true;
+}
+
+submitForm(): void {
+  console.log(this.userForm.value);
+  this.matchWindow = false;
+}
+
+get topics(): FormArray {
+  return this.userForm.get('topics') as FormArray;
+}
+
+addTopic(topic: string): void {
+  this.topics.push(this.fb.control(topic));
+}
 }
